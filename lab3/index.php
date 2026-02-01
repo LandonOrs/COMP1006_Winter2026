4a9dfbe77b22bfe139a00b4906909810312e3511
LandonOrs <student@local>
2026-02-01 16:52:40 -0500

    Add submit button and HTML5 validation (required/min/pattern); remove duplicate block

--- lab3/index.php
+++ lab3/index.php
@@ -1,21 +1,32 @@
-<hows this so far <?php require 'includes/header.php'; ?>
+<?php require 'includes/header.php'; ?>
 
- <main>
-
- <fieldset>
+<main>
+<form action="process.php" method="post">
+  <fieldset>
     <legend>Customer Information</legend>
 
-    <label for  ="first_name">First Name:</label>
-    <input type="text" name="first_name" id="first_name">
+    <label for="first_name">First Name:</label>
+    <input type="text" name="first_name" id="first_name" required maxlength="100"
+           value="<?php echo htmlspecialchars($_POST['first_name'] ?? ''); ?>" autocomplete="given-name">
 
-    <label for  ="last_name">Last Name:</label>
-    <input type="text" name="last_name" id="last_name">
+    <label for="last_name">Last Name:</label>
+    <input type="text" name="last_name" id="last_name" required maxlength="100"
+           value="<?php echo htmlspecialchars($_POST['last_name'] ?? ''); ?>" autocomplete="family-name">
 
-    <label for  ="email">Email:</label>
-    <input type="text" name="email" id="email">
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" required
+           value="<?php echo htmlspecialchars($_POST['email'] ?? ''); ?>" autocomplete="email">
 
-    <label for  ="phone">Phone Number:</label>
-    <input type="text" name="phone" id="phone">
-   
+    <label for="phone">Phone Number:</label>
+    <input type="tel" name="phone" id="phone" required pattern="^\d{3}-\d{3}-\d{4}$"
+           placeholder="555-123-4567"
+           value="<?php echo htmlspecialchars($_POST['phone'] ?? ''); ?>" autocomplete="tel">
+  </fieldset>
 
- </fieldset>
+ 
+  <button type="submit">Place Order</button>
+</form>
+</main>
+
+<?php require 'includes/footer.php'; ?>
+
